abdb8a17fba10b8e9831c0ab6e89ee6a16ed532b
3nzoo <florenzodinglasan@gmail.com>
2021-07-09 11:21:10 +0800

    updated phone number

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,12 @@
 module.exports = {
-    phoneNumber: process.env.phoneNumber,
+    phoneNumber: process.env.PHONENUMBER,
     password: process.env.PASSWORD,
-    region: process.env.REGION
+    region: process.env.REGION,
+    lamp:process.env.LAMP,
+    screen:process.env.SCREEN,
+    laptop:process.env.LAPTOP,
+    ref:process.env.REF,
+    harddrive:process.env.HARDDRIVE,
+    maCharger: process.env.MACHARGER,
+    charger: process.env.CHARGER
 };
