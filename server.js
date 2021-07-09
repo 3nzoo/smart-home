7032055fea5550be8368c760bcb0256967c0bfbc
3nzoo <florenzodinglasan@gmail.com>
2021-07-09 14:02:15 +0800

    added prod redirection

--- server.js
+++ server.js
@@ -99,6 +99,15 @@ app.get('/all',async (req,res) =>{
   }
 })
 
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 
 app.listen(port, ()=>{
   console.log(`connected to port ${port}`);
