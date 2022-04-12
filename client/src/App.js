81c1187511aaf9e635380fb56697adb9fb072bbb
3nzoo <florenzodinglasan@gmail.com>
2022-04-13 07:49:34 +0800

    updated chargingstatus

--- client/src/App.js
+++ client/src/App.js
@@ -3,32 +3,31 @@ import './App.css';
 
 import TitlebarImageList from './components/imageList';
 
-const laptopStatus = axios.get('laptop/status');
-
-console.log(laptopStatus.data);
-
 let chargerStat = false;
+
+axios.get('laptop/status').then((res) => {
+  if (res.data === 'on') {
+    chargerStat = true;
+  }
+});
 
 setInterval(() => {
   navigator.getBattery().then((battery) => {
     const level = Math.floor(battery.level * 100);
     if (level > 80 && chargerStat) {
-      axios.get('https://mysmarthomeph.herokuapp.com/laptop');
-      // axios.get('/laptop');
+      axios.get('/laptop');
       console.log('turned off: ', level);
       chargerStat = false;
     }
 
     if (level < 30 && !chargerStat) {
-      axios.get('https://mysmarthomeph.herokuapp.com/laptop');
-      // axios.get('/laptop');
+      axios.get('/laptop');
       console.log('turned on: ', level);
       chargerStat = true;
     }
-    // document.querySelector('.bat-status').textContent = level * 100;
     console.log(level);
   });
-}, 60000);
+}, 900000);
 
 function App() {
   return (
